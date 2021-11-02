3e7e2af5bf905aba5501a43057c2b412ba9b2d3a
Victoria Rossiyskaya <meowvoto@gmail.com>
2021-11-03 01:26:42 +0300

    added description for function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export function getPercents(percent, number) {
 	return number / 100 * percent;
 }
 
+// Тренажер по JavaScript -> Функции -> Задание 2.
+// Напишите функцию getPercents(percent, number), которая возвращает {percent} процентов от {number}.
